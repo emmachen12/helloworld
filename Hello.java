c2d2aeea6dc50798a2c6861d2d6db8c38bfe9020
emmachen12 <emc1207pi@gmail.com>
2024-02-06 14:51:29 -0500

    i finished my first program

---
+++ Hello.java
@@ -0,0 +1,17 @@
+ /**************************** *
+ * Project: Emma Chen
+ * Date: Febuary 6th
+ * Program Name: Hello.java
+ * ***********************/
+
+ public class Hello{
+
+     public static void main(String[] args){
+         System.out.println("Hello Everyone");
+         System.out.println("I wrote my first java program");
+     
+     
+     }
+
+
+ }
